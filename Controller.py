8c6f5bee9b06fdf0a24ed0c50801b560fabff4bc
Huzaifa Usmani <bitf19a007@pucit.edu.pk>
2021-11-16 10:49:03 +0500

    baki Controller.py mai password add kia

--- Controller.py
+++ Controller.py
@@ -1,7 +1,9 @@
 from Classes import *
 from Model import *
 
+
 class Controller:
     # CONSTRUCTOR
-    def __init__(self) -> None:
-        self.model = AddressBookModel("localhost", "root", "2334011019", "lab4")
+    def __init__(self, password) -> None:
+        self.password = password
+        self.model = Model("localhost", "root", password, "pharmacy")
