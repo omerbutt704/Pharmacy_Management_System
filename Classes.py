faa08528704f4aff892833189d34c0b66dfc2b6f
Huzaifa Usmani <bitf19a007@pucit.edu.pk>
2021-11-16 10:44:38 +0500

    apki Classes.py krdi mene

--- Classes.py
+++ Classes.py
@@ -1,41 +1,38 @@
 # ADMIN and CUSTOMER
-class User:
-    #                       member datatype          Function return type
-    #                             |                       |
-    def __init__(self, username: str, password: str) -> None:
-        self.username, self.password = username, password
+class Users:
+    def __init__(self, email: str, password: str, status: str) -> None:
+        self.email = email
+        self.password = password
+        self.status = status
 
 
-# MEDICINE
 class Medicine:
-    def __init__(self, name: str, formula: str, description: str, price: int, quantity: int) -> None:
-        self.name, self.formula = name, formula
-        self.description, self.price = description, price
+    def __init__(self, med_name: str, price: int, description: str, formula: str, quantity: int) -> None:
+        self.med_name = med_name
+        self.price = price
+        self.description = description
+        self.formula = formula
         self.quantity = quantity
 
 
-# INVENTORY
-class Inventory(Medicine):
-    def __init__(self, meds: list, name: str, formula: str, description: str, price: int, quantity: int) -> None:
-        super().__init__(name, formula, description, price, quantity)
-        self.medicine = meds
+class Inventory:
+    def __init__(self, list_of_Medicines={}):
+        self.med_obj = list_of_Medicines
 
-    def insert_medicine(self, med: Medicine) -> bool:
-        return True
-
-    def delete_medicine(self, med: Medicine) -> bool:
-        return True
-
-    def check_medicine(self, med: Medicine) -> bool:
-        return True
-
-    def check_quantity(self, med: Medicine) -> bool:
-        return True
-
-    # def alternative(self, formula: str) -> Medicine:
+    def get_all_medicine_details(self):
+        return self.med_obj
 
 
-# PRESCRIPTION
 class Prescription:
-    def __init__(self, name: str, quantity: str) -> None:
-        self.name, self.quantity = name, quantity
+    def __init__(self, med_name: str, quantity: str) -> None:
+        self.med_name, self.quantity = med_name, quantity
+
+
+class Billing:
+    def __init__(self, med_name, price, quantity):
+        self.med_name = med_name
+        self.price = price
+        self.quantity = quantity
+
+    def display(self):
+        print("Name: " + self.med_name + "\nPrice: " + self.price + "\nQuantity: " + self.quantity)
